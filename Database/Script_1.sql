0b2bcd187741ff942c4aa49b8186f8388e5b107c
Kittykras <Silqe@hotmail.com>
2014-03-28 10:50:40 +0100

    updaterede scripts

--- Database/Script_1.sql
+++ Database/Script_1.sql
@@ -5,9 +5,9 @@ use sedmuseum;
 
 drop table if exists product; 
 create table product
-(nr	int(3),
+(nr	int(3)	auto_increment primary key,
 p_name	varchar(30),
-p_group	varchar(30),
+p_group	int(3),
 supplyer	varchar(30),
 purchase_price	int,
 price_kr	double, 
@@ -18,13 +18,13 @@ amount int
 
 drop table if exists p_group; 
 create table p_group
-(nr	int(3),
+(nr	int(3)	auto_increment primary key,
 category	varchar(30)
 );
 
 drop table if exists employee;
 create table employee
-(nr	int(3),
+(nr	int(3)	auto_increment primary key,
 fname	varchar(20),
 lname	varchar(20),
 tlf_home	char(8),
@@ -37,14 +37,14 @@ city		varchar(20)
 
 drop table if exists invoice;
 create table invoice
-(nr	int,
+(nr	int	auto_increment primary key,
 i_date	date,
 employee	int(3)
 );
 
 drop table if exists till;
 create table till
-(t_date	date, 
+(t_date	date	primary key, 
 start_balance_kr	int,
 start_balance_euro	int
 );
--- Database/Script_2.sql
+++ Database/Script_2.sql
@@ -1,7 +1,29 @@
 use sedmuseum;
 
+set foreign_key_checks = 0;
+
 drop table if exists invoicelines;
 create table invoicelines
+<<<<<<< HEAD
+(invoice_nr	int,
+product_nr	int(3),
+amount	int,
+valuta	int,
+primary key(invoice_nr, product_nr),
+foreign key(invoice_nr) references invoice(nr),
+foreign key(product_nr) references product(nr)
+)engine = InnoDB;
+
+alter table product
+add foreign key(p_group) references p_group(nr),
+engine = innodb;
+
+alter table invoice
+add foreign key(employee) references employee(nr),
+add foreign key(i_date) references till(t_date),
+engine = innodb;
+=======
 (amount	int,
 currency	varchar(3)
 );
+>>>>>>> 244c5af97283d311a9cdeb4e05243b5f39d56ccd
