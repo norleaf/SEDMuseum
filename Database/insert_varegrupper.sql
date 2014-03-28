63954939e04601973826eac4a888f3635c5dd670
Friedaz <mugglewithasecret@hotmail.com>
2014-03-28 11:53:00 +0100

    Nu bør den være der. (insert varegrupper)

--- Database/insert_varegrupper.sql
+++ Database/insert_varegrupper.sql
@@ -1,28 +1,27 @@
-use sedmuseum;
-
-insert into p_group(category)
-values
-('Bøger'),
-('Hæfter'),
-('Legetøj'),
-('Café'), 
-('Diverse'), 
-('Smykker'), 
-('Mobiler'), 
-('Postkort'),
-('Museums Glas'),
-('Kâhler Keramik'),
-('Holmegaard'),
-('Katalog'), 
-('Plakat'),
-('Mjød'), 
-('Øl'), 
-('Weibel'), 
-('Klippeark');
-
-insert into product
-values
-(1210, 'Liv og Levn 1 (kopi)', 2, null, null, 40.00, 6.00, 36.00, 5.00,10),
-(1211, 'Liv og Levn 2 (kopi)',2, null, null, 40.00,6.00,36.00, 5.00,10),
-(4320, 'Aktuel Plakat', 13, null, null, 10.00,1.50,9.00,1.00,10),
-(1379,'Tidstavle', 13, null, null, 75.00, 10.00, 67.50, 9.00,10);
+	use sedmuseum;
+	
+	insert into p_group(category)
+	values
+	('Bøger'),
+	('Hæfter'),
+	('Legetøj'),
+	('Café'), 
+	('Diverse'), 
+	('Smykker'), 
+	('Mobiler'), 
+	('Postkort'),
+	('Museums Glas'),
+	('Kâhler Keramik'),
+	('Holmegaard'),
+	('Katalog'), 
+	('Plakat'),
+	('Mjød'), 
+	('Øl'), 
+	('Weibel'), 
+	('Klippeark');
+	
+	insert into product
+	values	(1210, 'Liv og Levn 1 (kopi)', 2, null, null, 40.00, 6.00, 36.00, 5.00,10),
+	(1211, 'Liv og Levn 2 (kopi)',2, null, null, 40.00,6.00,36.00, 5.00,10),
+	(4320, 'Aktuel Plakat', 13, null, null, 10.00,1.50,9.00,1.00,10),
+	(1379,'Tidstavle', 13, null, null, 75.00, 10.00, 67.50, 9.00,10);
