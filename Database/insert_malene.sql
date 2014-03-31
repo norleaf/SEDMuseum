b2e664129a588ed179d30edd013d8ddc8cdf81b9
Friedaz <mugglewithasecret@hotmail.com>
2014-03-31 10:04:25 +0200

    malene - varer

---
+++ Database/insert_malene.sql
@@ -0,0 +1,18 @@
+insert into product
+values
+(2262, 'Saltkar med ske/horn', 5, null, null, 115.00, 16.00, 103.50, 14.00, 10), 
+(2298, 'Sæbe - mandel', 5, null, null, 20.00, 3.00, 18.00, 2.50, 10), 
+(3062, 'Hellig Tre Konger Lys', 5, null, null, 40.00, 5.50, 36.00, 5.00, 10),
+(2185, 'Nipsenål', 5, null, null, 15.00, 2.00, 13.50, 1.80, 10), 
+(2186, 'Nipsenåle', 5, null, null, 15.00,2.00,13.50,1.80,10), 
+(2111, 'Dannebro Ko', 5, null, null, 40.00, 5.50,  36.00, 2.50, 10), 
+(2112, 'Dannebro Blyant', 5, null, null, 8.00, 1.50, 7.50, 1.00, 10), 
+(2110, 'Drikkehorn', 5, null, null, 120.00, 16.00, 108.00, 15.00, 10), 
+(2360, 'Kähler USB-stik', 5, null, null, 399.00, 54.00, 359.00, 48.00, 10),
+(4015, 'Dorte Dukker m.fl.', 17, null, null, 20.00, 3.00, 18.00, 2.50, 10), 
+(4150, 'Glansbilleder', 17, null, null, 10.00, 1.50, 9.00, 1.00, 10), 
+(4061, 'Store og lille Marie dukker', 17, null, null, 10.00, 1.50,9.00,1.00, 10), 
+(4062, 'Store og lille Marie tøj', 17, null, null, 19.00, 3.00, 17.00, 2.50,10), 
+(2258, 'Chokolade i metalæske', 16, null, null, 42.00, 6.00, 37.50, 5.00, 10);
+
+-- diverse er 5.
